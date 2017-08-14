2db2d6830643bb414005b17f5bc278f9824984c4
Guilherme Biegelmeyer <guilhermebiegelmeyer@gmail.com>
2017-08-13 22:21:24 -0300

    adicionado a nova home do site - com todos os ajustes

--- home/index.php
+++ home/index.php
@@ -7,10 +7,31 @@
 			<label class="pull-right">Quem <label class="cor2">Somos</label></label>
 		</div>
 
+
+		<div class="col-lg-5">
+			<label class="pull-right">Quem <label class="cor2">Somos</label></label>
+		</div>
+
+		<div class="col-lg-5">
+			<label class="pull-right">Quem <label class="cor2">Somos</label></label>
+		</div><div class="col-lg-5">
+			<label class="pull-right">Quem <label class="cor2">Somos</label></label>
+		</div>
+	
 		<div class="col-lg-6">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor  agagdgdsgd gdgdsin reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
 
 			<button type="button" class="verMais">Ver Mais</button>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-5 align-center">
+			<label class="servicosProdutos">Serviços &<br><label class="cor2">Produtos</label></label>
+		</div>
+
+		<div class="col-lg-6">
+			&nbsp;
 		</div>
 	</div>
 
@@ -59,6 +80,32 @@
 			</div>
 		</div>	
 	</div>
+
+	<div class="col-lg-6 encurtaTxtChamada">
+			<label class="tituloChamada">Colchões</label>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+			</p>
+			<div class="col-xs-12 text-center">
+				<button type="button" class="verMaisChamada">Ver Mais</button>
+			</div>
+		</div><div class="col-lg-6 encurtaTxtChamada">
+			<label class="tituloChamada">Colchões</label>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+			</p>
+			<div class="col-xs-12 text-center">
+				<button type="button" class="verMaisChamada">Ver Mais</button>
+			</div>
+		</div><div class="col-lg-6 encurtaTxtChamada">
+			<label class="tituloChamada">Colchões</label>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+			</p>
+			<div class="col-xs-12 text-center">
+				<button type="button" class="verMaisChamada">Ver Mais</button>
+			</div>
+		</div>
 
 	<div class="row separaChamada">
 		<div class="col-lg-6">
