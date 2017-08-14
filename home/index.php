7bd7306a7e8c6b7828c95e45e1bbccdf2c74ee63
Guilherme Biegelmeyer <guilhermebiegelmeyer@gmail.com>
2017-08-13 22:34:52 -0300

    teste de homes, conflito?

--- home/index.php
+++ home/index.php
@@ -35,16 +35,6 @@
 		</div>
 	</div>
 
-	<div class="row">
-		<div class="col-lg-5 align-center">
-			<label class="servicosProdutos">Serviços &<br><label class="cor2">Produtos</label></label>
-		</div>
-
-		<div class="col-lg-6">
-			&nbsp;
-		</div>
-	</div>
-
 	<div class="row separaChamada">
 		<div class="col-lg-6">
 			<div class="pull-right suportaImage" style="background-image: url('../imagem/chamada.png');">
@@ -84,17 +74,6 @@
 	<div class="col-lg-6 encurtaTxtChamada">
 			<label class="tituloChamada">Colchões</label>
 			<label class="tituloChamada">Colchões</label>
-			<label class="tituloChamada">Colchões</label>
-			<label class="tituloChamada">Colchões</label>
-			<label class="tituloChamada">Colchões</label>
-			<label class="tituloChamada">Colchões</label>
-			<label class="tituloChamada">Colchões</label>
-			<label class="tituloChamada">Colchões</label>
-			<label class="tituloChamada">Colchões</label>
-			<label class="tituloChamada">Colchões</label>
-			<label class="tituloChamada">Colchões</label>
-			<label class="tituloChamada">Colchões</label>
-			<label class="tituloChamada">Colchões</label>aaa
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 			</p>
