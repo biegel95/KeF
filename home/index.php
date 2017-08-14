f37219072968d16a48adea2a9a9e5f88048ce3e0
Guilherme Biegelmeyer <guilhermebiegelmeyer@gmail.com>
2017-08-13 21:50:31 -0300

    Inclusão da home do site

---
+++ home/index.php
@@ -0,0 +1,82 @@
+<?php include("../includes/topo.php"); ?>
+<?php include("../includes/banner.php"); ?>
+
+<div class="container chamadas">
+	<div class="row quemSomosHome">
+		<div class="col-lg-5">
+			<label class="pull-right">Quem <label class="cor2">Somos</label></label>
+		</div>
+
+		<div class="col-lg-6">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+
+			<button type="button" class="verMais">Ver Mais</button>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-5 align-center">
+			<label class="servicosProdutos">Serviços &<br><label class="cor2">Produtos</label></label>
+		</div>
+
+		<div class="col-lg-6">
+			&nbsp;
+		</div>
+	</div>
+
+	<div class="row separaChamada">
+		<div class="col-lg-6">
+			<div class="pull-right suportaImage" style="background-image: url('../imagem/chamada.png');">
+				&nbsp;
+			</div>
+		</div>
+
+		<div class="col-lg-6 encurtaTxtChamada">
+			<label class="tituloChamada">Colchões</label>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+			</p>
+			<div class="col-xs-12 text-center">
+				<button type="button" class="verMaisChamada">Ver Mais</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="row separaChamada">
+		<div class="col-lg-6 text-right encurtaTxtChamada">
+			<label class="tituloChamada">Colchões</label>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+			</p>
+			<div class="col-xs-12 text-center">
+				<button type="button" class="verMaisChamada">Ver Mais</button>
+			</div>
+		</div>
+
+		<div class="col-lg-6">
+			<div class="suportaImage" style="background-image: url('../imagem/chamada.png');">
+				&nbsp;
+			</div>
+		</div>	
+	</div>
+
+	<div class="row separaChamada">
+		<div class="col-lg-6">
+			<div class="pull-right suportaImage" style="background-image: url('../imagem/chamada.png');">
+				&nbsp;
+			</div>
+		</div>
+
+		<div class="col-lg-6 encurtaTxtChamada">
+			<label class="tituloChamada">Colchões</label>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+			</p>
+			<div class="col-xs-12 text-center">
+				<button type="button" class="verMaisChamada">Ver Mais</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include("../includes/rodape.php"); ?>
