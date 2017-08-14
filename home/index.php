702887250965b5eba81ca3cd26481cfe28404504
Guilherme Biegelmeyer <guilhermebiegelmeyer@gmail.com>
2017-08-13 22:28:11 -0300

    ajustado a home do site

--- home/index.php
+++ home/index.php
@@ -83,6 +83,18 @@
 
 	<div class="col-lg-6 encurtaTxtChamada">
 			<label class="tituloChamada">Colchões</label>
+			<label class="tituloChamada">Colchões</label>
+			<label class="tituloChamada">Colchões</label>
+			<label class="tituloChamada">Colchões</label>
+			<label class="tituloChamada">Colchões</label>
+			<label class="tituloChamada">Colchões</label>
+			<label class="tituloChamada">Colchões</label>
+			<label class="tituloChamada">Colchões</label>
+			<label class="tituloChamada">Colchões</label>
+			<label class="tituloChamada">Colchões</label>
+			<label class="tituloChamada">Colchões</label>
+			<label class="tituloChamada">Colchões</label>
+			<label class="tituloChamada">Colchões</label>
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 			</p>
